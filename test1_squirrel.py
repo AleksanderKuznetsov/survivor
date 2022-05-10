9cc432f152b622fb1f7d77b7d0f13603edc9e713
AleksanderKuznetsov <0504728348a@gmail.com>
2022-05-10 17:39:25 +0300

    add test1_squirrel.py

---
+++ test1_squirrel.py
@@ -0,0 +1,19 @@
+"""
+Function - find the first digit of a factorial
+"""
+
+
+def squirrel(nuts: int) -> int:
+    """
+    :param nuts: non-negative integer
+    :return: first digit of a factorial
+    """
+    # Factorial must start at 1.
+    factorial = 1
+    # Finding the factorial.
+    for i in range(2, nuts+1):
+        factorial *= i
+    # Change integer to str type.
+    type_str = str(factorial)
+
+    return type_str[0]
