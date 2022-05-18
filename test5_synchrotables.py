0fac81fb5f5485d8bc1ad3094bc5d212101ea4e6
AleksanderKuznetsov <0504728348a@gmail.com>
2022-05-18 19:56:19 +0300

    add test5_synchrotables.py

---
+++ test5_synchrotables.py
@@ -0,0 +1,21 @@
+"""
+sorting list 2 according to the shuffled sorting of list 1 without changing list 1
+"""
+
+
+def SynchronizingTables(N: int, ids: list, salary: list) -> list:
+    """
+    :param N: the length of both lists
+    :param ids: list 1 - keys that cannot be changed
+    :param salary: list 2 - to be sorted
+    :return: sorted list 2
+    """
+
+    for z in range(2):
+        for i in range(N):
+            for j in range(N):
+                if ids[i] > ids[j] and salary[i] < salary[j]:
+                    salary[i], salary[j] = salary[j], salary[i]
+                elif ids[i] < ids[j] and salary[i] > salary[j]:
+                    salary[i], salary[j] = salary[j], salary[i]
+    return salary
