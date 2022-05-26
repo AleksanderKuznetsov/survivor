eaee8ba59eba4acde991680b74268efcb0614d9d
AleksanderKuznetsov <0504728348a@gmail.com>
2022-05-26 09:53:32 +0300

    edit test7_wordsearch.py-error

--- test7_wordsearch.py
+++ test7_wordsearch.py
@@ -20,8 +20,9 @@ def WordSearch(page_width: int, str_start: str, subs: str) -> list:
 
     for word in array_start:
         len_word = len(word)
+        iteration += 1
         # Word is longer than line width and word is first.
-        if len_word > page_width and symbols == 0:
+        if len_word > page_width and symbols == 0 and len(array_start) - iteration != 0:
             # Split the word.
             s1 = word[:page_width - len_word]
             s2 = word[page_width - len_word:]
@@ -30,6 +31,17 @@ def WordSearch(page_width: int, str_start: str, subs: str) -> list:
             array_stl = []  # Zero out the array.
             array_stl.append(s2)
             symbols = len(s2) + 1
+
+        if len_word > page_width and symbols == 0 and len(array_start) - iteration == 0:
+            # Split the word.
+            s1 = word[:page_width - len_word]
+            s2 = word[page_width - len_word:]
+            array_stl.append(s1)
+            array_str.append(array_stl)
+            array_stl = []  # Zero out the array.
+            array_stl.append(s2)
+            array_str.append(array_stl)
+
 
         # The word is longer than the line width and the word is NOT the first.
         elif len_word > page_width and symbols > 0:
@@ -45,30 +57,28 @@ def WordSearch(page_width: int, str_start: str, subs: str) -> list:
             symbols = len(s2) + 1
 
         #  Current word + length of previous < page width. The word is NOT the last
-        elif len_word + symbols <= page_width and len(array_start) - iteration != 1:
+        elif len_word + symbols <= page_width and len(array_start) - iteration != 0:
             array_stl.append(word)
             symbols += len_word + 1
 
         #  Current word + length of previous < page width. The word is the last
-        elif len_word + symbols <= page_width and len(array_start) - iteration == 1:
+        elif len_word + symbols <= page_width and len(array_start) - iteration == 0:
             array_stl.append(word)
             array_str.append(array_stl)
 
         #  Current word + length of words in this line > page width. The word is NOT the last.
-        elif len_word + 1 + symbols > page_width and len(array_start) - iteration != 1:
+        elif len_word + 1 + symbols > page_width and len(array_start) - iteration != 0:
             array_str.append(array_stl)
             symbols = len_word + 1
             array_stl = [] # Zero out the array.
             array_stl.append(word)
 
         #  Current word + length of words in this line > page width. The word is the last.
-        elif len_word + 1 + symbols > page_width and len(array_start) - iteration == 1:
+        elif len_word + 1 + symbols > page_width and len(array_start) - iteration == 0:
             array_str.append(array_stl)
             array_stl = [] # Zero out the array.
             array_stl.append(word)
             array_str.append(array_stl)
-
-        iteration += 1
 
     # Looking for substring matches.
     for line in array_str:
