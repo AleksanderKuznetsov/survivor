32d964393dce4cdfcf52b56351eedf53cbc15750
AleksanderKuznetsov <0504728348a@gmail.com>
2022-05-27 07:05:57 +0300

    add test8_sumofthe

--- test8_sumofthe.py
+++ test8_sumofthe.py
@@ -5,9 +5,9 @@ find a number from the list - the sum of the remaining numbers
 
 def SumOfThe(numbers: int, data: list) -> int:
     """
-    :param numbers: summary length
-    :param data: array of numbers
-    :return: a number from the array equal to the sum of the other numbers
+    :param numbers: summary length.
+    :param data: array of numbers.
+    :return: a number from the array equal to the sum of the other numbers.
     """
     for i in range(numbers):
         summa = 0
