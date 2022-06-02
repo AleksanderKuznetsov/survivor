08ca481a25c8e75fdb021583aeea4297c55aef98
AleksanderKuznetsov <0504728348a@gmail.com>
2022-06-02 08:57:12 +0300

    edit test11_bigminus.py -new algorithm

--- test11_bigminus.py
+++ test11_bigminus.py
@@ -5,12 +5,36 @@
 
 def BigMinus(line1: str, line2: str) -> str:
     """
+    calculate in a column
     :param line1: first number in str
     :param line2: second number in str
     :return: difference in str
     """
-    if int(line1) - int(line2) > 0:
-        result = int(line1) - int(line2)
-    else:
-        result = int(line2) - int(line1)
-    return str(result)
+    flag = False  # if -1 from the next number: True.
+    array_ = []
+
+    # Find a long line and make it line1.
+    if len(line1) < len(line2):
+        line1, line2 = line2, line1
+    elif len(line1) == len(line2) and int(line2[0]) > int(line1[0]):
+        line1, line2 = line2, line1
+
+    # Adding "0" to the beginning of a short string.
+    line2 = "0" * (len(line1) - len(line2)) + line2
+
+    # Cycle through line1 from the end to the beginning.
+    for i in range(len(line1)-1, -1, -1):
+        if int(line1[i]) - int(line2[i]) < 0 and not flag:
+            result = int(line1[i]) + 10 - int(line2[i])
+            flag = True
+        elif int(line1[i]) - int(line2[i]) <= 0 and flag:
+            result = int(line1[i]) + 9 - int(line2[i])
+        elif flag:
+            result = int(line1[i]) - 1 - int(line2[i])
+            flag = False
+        else:
+            result = int(line1[i]) - int(line2[i])
+            flag = False
+        array_.insert(0, str(result))
+
+    return "".join(array_)
