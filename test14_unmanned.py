e7eb83081b147bf5c95ffd9b9dc51ee56a6cd6e8
AleksanderKuznetsov <0504728348a@gmail.com>
2022-06-11 11:24:42 +0300

    add test14_unmanned.py

---
+++ test14_unmanned.py
@@ -0,0 +1,30 @@
+"""
+Determine how long it will take the car to reach the end of the road
+"""
+
+def Unmanned(road: int, lights: int, track: list) -> int:
+    """
+    :param road: road length.
+    :param lights: number of traffic lights.
+    :param track: [0] - traffic light start
+                  [1] - red light, seconds
+                  [2] - green light, seconds
+    :return: travel time
+    """
+    dist = 0
+    time = 0
+    # The loop runs until it completes road.
+    while dist < road:
+        for i in range(lights):
+            if dist == track[i][0]:
+                # If the remainder of the division of time and the sum of
+                # lights is less than the time of red, then red.
+                while time % (track[i][1]+track[i][2]) < track[i][1]:
+                    time += 1
+        time += 1
+        dist += 1
+
+    return time
+
+
+print(Unmanned(10, 2, [[3, 5, 5], [5, 2, 2]]))
